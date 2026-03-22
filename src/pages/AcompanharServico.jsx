59e7c2a7fd00d5aa520b55f40f0ddd7a41431124
Pratica Escola <escolapraticabetim@gmail.com>
2026-03-22 23:57:48 +0000

    File changes

--- src/pages/AcompanharServico.jsx
+++ src/pages/AcompanharServico.jsx
@@ -49,6 +49,25 @@ export default function AcompanharServico() {
         playNotificationSound();
       }
     }
+  const playNotificationSound = () => {
+    // Create a simple beep sound using Web Audio API
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+    
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    
+    oscillator.frequency.value = 800;
+    oscillator.type = 'sine';
+    
+    gainNode.gain.setValueAtTime(0.3, audioContext.currentTime);
+    gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.5);
+    
+    oscillator.start(audioContext.currentTime);
+    oscillator.stop(audioContext.currentTime + 0.5);
+  };
+
   const { data: request } = useQuery({
     queryKey: ['service-request', id],
     queryFn: async () => {
