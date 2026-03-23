c088eac142e21764fcafab17d8858f97e755866c
Pratica Escola <escolapraticabetim@gmail.com>
2026-03-23 00:13:20 +0000

    File changes

--- src/pages/AcompanharServico.jsx
+++ src/pages/AcompanharServico.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
