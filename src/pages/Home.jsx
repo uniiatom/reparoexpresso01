26df7e76ca1f83f4b955f646aec9990b009e9f47
Pratica Escola <escolapraticabetim@gmail.com>
2026-05-03 06:22:05 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -134,7 +134,7 @@ export default function Home() {
             transition={{ duration: 0.4 }}
           >
             <motion.img
-              src="https://media.base44.com/images/public/69bdfd09a4593d6a3b1890df/5bfa5c45b_generated_image.png"
+              src="https://media.base44.com/images/public/69bdfd09a4593d6a3b1890df/619e5b049_generated_image.png"
               alt="Reparo Expresso"
               initial={{ opacity: 0, scale: 0.9 }}
               animate={{ opacity: 1, scale: 1 }}
